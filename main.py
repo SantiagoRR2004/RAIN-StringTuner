e0284e4305de743da892d1a488f4f6569b6e4d78
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-09-23 21:35:17 +0200

    String formulas and some refactoring

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import numpy as np
 import sounddevice as sd
 
-frecuencies = [329.63, 246.94, 196.00, 146.83, 110.00, 82.41]
 
-for frequency in frecuencies:
+def calculateStringFrequency(
+    length: float, tension: float, massPerLegth: float
+) -> float:
+    """
+    This formula is extracted from the following link:
+    https://en.wikipedia.org/wiki/Mersenne%27s_laws
+
+    It calculates the frequency of a string given its length, tension and mass per length.
+
+    Args:
+        - length (float): The length of the string in meters.
+        - tension (float): The tension or force of the string in newtons.
+        - massPerLegth (float): The mass per length of the string in
+            kilograms per meter. https://en.wikipedia.org/wiki/Linear_density
+
+    Returns:
+        - float: The frequency of the string in hertz.
+    """
+    return np.sqrt(tension / massPerLegth) / (2 * length)
+
+
+def playCordFrequency(frequency: float) -> None:
+    """
+    Play a sound with the given frequency using the Karplus-Strong algorithm.
+    https://en.wikipedia.org/wiki/Karplus%E2%80%93Strong_string_synthesis
+
+    We also got this code from the following link:
+    https://github.com/earthspecies/intro-to-DSP-with-python/blob/main/02_Generate_a_sound_that_resembles_violin.ipynb
+    and had ChatGPT modify it to change the frecuency of the sound.
+
+    Args:
+        - frequency (float): The frequency of the sound to play.
+
+    Returns:
+        - None
+    """
     sr = 16000  # Sample rate
     decay = 0.99  # Feedback decay factor
     duration = 2  # Duration in seconds
@@ -32,3 +66,44 @@ for frequency in frecuencies:
     # Play the synthesized sound
     sd.play(signal, samplerate=sr)
     sd.wait()  # Wait until the audio is done playing
+
+
+def calculateStringTension(
+    elasticModulus: float, crossSection: float, originalLength: float, newLength: float
+) -> float:
+    """
+    This formula appears in the "Tensional stress of a uniform bar"
+    of the following link:
+    https://en.wikipedia.org/wiki/Hooke%27s_law#Tensional_stress_of_a_uniform_bar
+
+    Args:
+        - elasticModulus (float): The elastic modulus of the string in pascals.
+            Specifically it is the Young's modulus:
+            https://en.wikipedia.org/wiki/Elastic_modulus
+            https://en.wikipedia.org/wiki/Young%27s_modulus
+        - crossSection (float): The cross section of the string in square meters.
+        - originalLength (float): The original length of the string in meters.
+        - newLength (float): The new length of the string in meters.
+
+    Returns:
+        - float: The tension of the string in newtons.
+    """
+    return elasticModulus * crossSection * (newLength - originalLength) / originalLength
+
+
+if __name__ == "__main__":
+    # https://en.wikipedia.org/wiki/Guitar_tunings
+    frecuencies = [329.63, 246.94, 196.00, 146.83, 110.00, 82.41]
+
+    for f in frecuencies:
+        playCordFrequency(f)
+
+    # https://en.wikipedia.org/wiki/Audio_frequency
+    maxFrecuency = 20000
+    minFrecuency = 20
+
+    # https://en.wikipedia.org/wiki/Psychoacoustics
+    frequencyDiscrimination = 3.6
+
+    # https://en.wikipedia.org/wiki/Scale_length_(string_instruments)
+    guitarLength = 0.65
