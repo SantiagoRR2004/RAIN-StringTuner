99b2bf1165f685cc7c5e82f5006b30bf0804900c
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-11-06 09:25:51 +0100

    Created 2 general tests

---
+++ tests/test_instrument.py
@@ -0,0 +1,45 @@
+import unittest
+import random
+import guitars
+import harplike
+
+
+class numTests(unittest.TestCase):
+    numTests = 100
+
+
+class test_instrument(numTests):
+
+    def test_5TurnsGuitar(self):
+        """
+        Checks if a guitar can be tuned in only 5 turns
+        for each string. Each string is very close to the
+        objective or very far from it.
+        """
+
+        for _ in range(self.numTests):
+            guitar = guitars.ClassicalGuitar()
+
+            objective = guitar.frequencies
+            actual = guitar.stringFrequencies
+
+            for i in range(len(objective)):
+                if random.random() < 0.5:
+                    actual[i] = max(0, objective[i] + random.uniform(-5, 5))
+                else:
+                    actual[i] = max(0, objective[i] + random.uniform(-500, 500))
+
+            guitar.calculateTightness()
+
+            self.assertLessEqual(len(guitar.tune()), 5)
+
+    def test_12TurnsHarp(self):
+        """
+        Checks if a harp can be tuned in only 12 turns.
+        We start with a harp that doesn't have any of
+        the strings tightened.
+        """
+
+        harp = harplike.Harp36String()
+
+        self.assertLessEqual(len(harp.tune()), 12)
